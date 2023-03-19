c520f30f5bb44a942e07fc51f631d5f770c62699
GitHubのユーザー名 <GitHub>
2023-03-19 14:31:29 +0900

    バリデーション機能の実装

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,5 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory; 
+    
+    public function getPaginateByLimit(int $limit_count = 10)
+    {
+    // updated_atで降順に並べたあと、limitで件数制限をかける
+    return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
+    }
+    
+    protected $fillable = [
+        'title',
+        'body',
+        ];
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PostController;  //外部にあるPostControllerクラスをインポート。
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,8 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-use App\Http\Controllers\PostController;  //外部にあるPostControllerクラスをインポート。
 
+Route::get('/', [PostController::class, 'index']); 
+Route::get('/posts/create', [PostController::class, 'create']);
+Route::get('/posts/{post}', [PostController::class ,'show']);
+Route::post('/posts', [PostController::class, 'store']);
 
-Route::get('/posts', [PostController::class, 'index']);   
 ?>
